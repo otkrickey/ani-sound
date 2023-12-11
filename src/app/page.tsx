3436f5f3db3af8eb3050b17ba0a89c2614e78cbc
Baketsu150L <7322024@ed.tus.ac.jp>
2023-12-11 15:46:34 +0900

    Add audio_test page and configure next.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-// export default function Home() {
-
-//   return (
-//     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-//       <h1 className="text-4xl md:text-6xl font-bold text-center mx-4">
-//         Ani Sound
-//       </h1>
-//       <p className="mt-4 text-xl md:text-2xl text-center mx-6">
-//         Web Audio APIによって作られた音とアニメーションを合わせたマルチメディアコンテンツ
-//       </p>
-//       <button
-//         className="mt-8 px-6 py-3 text-xl md:px-10 md:py-4 md:text-3xl bg-gradient-to-r from-green-400 to-blue-500 rounded-full text-white shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-300"
-//       >
-//         {"Let's Start"}
-//       </button>
-//     </main>
-//   );
-// }
-
 "use client";
 import { useState } from 'react';
 
@@ -44,6 +25,9 @@ export default function Home() {
       <button
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
+        onClick={() => {
+          location.href = '/audio_test';
+        }}
         className={`mt-8 px-6 py-3 text-xl md:px-10 md:py-4 md:text-3xl bg-gradient-to-r from-green-400 to-blue-500 rounded-full text-white shadow-lg ${isTouched ? 'scale-110' : 'hover:scale-110'
           } transform transition duration-300`}
       >
